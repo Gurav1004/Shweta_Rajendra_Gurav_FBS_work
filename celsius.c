dc9134f96c4408e231c1bfac591aad46655b9241
Shweta Gurav <144773010+Gurav1004@users.noreply.github.com>
2026-07-20 23:45:06 +0530

    Add files via upload

---
+++ celsius.c
@@ -0,0 +1,9 @@
+void main()
+{
+     float c,f;
+	printf("Celsius :");
+	scanf("%f", &c);
+	
+	f = (c * 9 / 5) + 32;
+	printf("fahrenheit: %f", f);
+}
---
+++ triangle.c
@@ -0,0 +1,12 @@
+void  main()
+{
+	int base,height,area;
+	 printf("enter base : ");
+	 scanf("%d", &base);
+	 
+	 printf("enter height: ");
+	 scanf("%d", &height);
+	 
+	 area = 0.5  * base* height;
+	 printf(" area = %d", area);
+}
