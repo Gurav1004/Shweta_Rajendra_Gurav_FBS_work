6f005cf12dc441cb679356b307f2c58f4813fe94
Shweta Gurav <144773010+Gurav1004@users.noreply.github.com>
2026-08-17 00:24:32 +0530

    Add files via upload

---
+++ Assignments/assignment 5/que3.c
@@ -0,0 +1,14 @@
+void main()
+{
+	int n = 5;
+	for(int i = 1; i<=5; i++)
+	{
+		for(int j = n; j>=i; j--)
+		{
+		    printf("*");	
+		}
+		   printf("\n");
+		   
+	}
+	      
+}
---
+++ Assignments/assignment 5/que4.c
@@ -0,0 +1,24 @@
+void main()
+{
+	int n = 5;
+	for(int i = 1; i<=n; i++)
+	{
+		for(int j = n; j>=i; j-- )
+		{
+			printf("  ");
+		}
+			for(int j=1; j<=i ;j++)
+		{
+			printf("* ");
+		
+     	}
+     	  for(int j = 2; j<=i; j++ )
+		{
+			printf("* ");
+		}
+		    printf("\n");
+		   
+	}
+	
+	 
+}
